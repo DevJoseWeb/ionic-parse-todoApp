c2f43e93bb114b6e44ab8964583dfc86f050c7ce
Vitor Lobao <vitor.lobs@gmail.com>
2015-06-19 03:17:08 -0300

    todoApp

---
+++ www/js/app.js
@@ -0,0 +1,41 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('todoApp', ['ionic','todoApp.controllers','todoApp.services'])
+
+.run(function($ionicPlatform,$state) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+    $state.go('todos');
+  });
+})
+
+.value('PARSE_CREDENTIALS',{
+    APP_ID: 'jJOD5nSOzwUYMOskBfcXUKTrHvUWA2t39Ky14g77',
+    REST_API_KEY:'OmQvRmggXWVXHznnFleAPXK3bjE5z0k3PKAt0TU5'
+})
+
+.config(function($stateProvider){
+        $stateProvider.state('todos',{
+           url:'/todos',
+           controller:'TodoListController',
+           templateUrl:'views/todos.html'
+        }).state('createTodo',{
+            url:'/todo/new',
+            controller:'TodoCreationController',
+            templateUrl:'views/create-todo.html'
+        }).state('editTodo',{
+            url:'/todo/edit/:id/:content',
+            controller:'TodoEditController',
+            templateUrl:'views/edit-todo.html'
+        });
+}); 
